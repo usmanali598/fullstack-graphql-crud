ee511ee66702720f93579a981bd44cec18c654b7
Usman Ali <usman_pervaiz@live.com>
2021-06-05 20:23:04 +0300

    refactoring

--- client/src/BooksQuery.js
+++ client/src/BooksQuery.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Table, Form, FormGroup, Label,
-  Modal, ModalHeader, ModalBody, ModalFooter, Input } from 'reactstrap';
+import { Button, Table, Form, FormGroup, Label, Modal, ModalHeader, ModalBody, ModalFooter, Input } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const BooksQuery = (props)=> {
+const BooksQuery = ()=> {
   const [books, setBooks] = useState();
   const [name, setName] = useState('');
   const [author, setAuthor] = useState('');
   const [updateId, setUpdateId] = useState('');
   const [updateName, setUpdateName] = useState('');
+  const [updateAuthorId, setUpdateAuthorId] = useState('');
   const [updateAuthor, setUpdateAuthor] = useState('');
 
   const [modal, setModal] = useState(false);
@@ -18,9 +18,11 @@ const BooksQuery = (props)=> {
 
     if (e.target.textContent.toLowerCase() == 'update'){
       let nodes = e.target.parentNode.parentNode;
+      console.log(nodes, 'nodes')
       nodes.children[0] && setUpdateId(nodes.children[0].textContent);
       nodes.children[1] && setUpdateName(nodes.children[1].textContent);
-      nodes.children[2] && setUpdateAuthor(nodes.children[2].textContent);
+      nodes.children[2] && setUpdateAuthorId(nodes.children[2].textContent);
+      nodes.children[3] && setUpdateAuthor(nodes.children[3].textContent);
     }
 
     if (e.target.textContent.toLowerCase() == 'save'){
@@ -30,15 +32,6 @@ const BooksQuery = (props)=> {
 
     setModal(!modal)
   };
-  const changeUnmountOnClose = e => {
-    let value = e.target.value;
-    setUnmountOnClose(JSON.parse(value));
-  }
-
-  const {
-    buttonLabel,
-    className
-  } = props;
 
   const addBook = () => {
     fetch('http://localhost:5000/graphql', {
@@ -52,7 +45,9 @@ const BooksQuery = (props)=> {
       })
     })
       .then(r => r.json())
-      .then(data => console.log('data returned:', data));
+      // .then(data => console.log('data returned:', data));
+      setName('');
+      setAuthor('');
   }
 
   const updateBook = () => {
@@ -63,11 +58,11 @@ const BooksQuery = (props)=> {
         'Accept': 'application/json',
       },
       body: JSON.stringify({
-        "query": `mutation{ updateBook(id:${Number(updateId)} , name:"${updateName}", authorId:${Number(updateAuthor)}){id name authorId} }`
+        "query": `mutation{ updateBook(id:${Number(updateId)} , name:"${updateName}", authorId:${Number(updateAuthorId)}){id name authorId} }`
       })
     })
       .then(r => r.json())
-      .then(data => console.log('data returned:', data));
+      // .then(data => console.log('data returned:', data));
   }
 
   const getBooks = () => {
@@ -99,7 +94,7 @@ const BooksQuery = (props)=> {
       body: JSON.stringify({ "query": `mutation{ deleteBook(id:${id}){id} }` })
     })
       .then(r => r.json())
-      .then(data => console.log('data returned:', data));
+      // .then(data => console.log('data returned:', data));
   }
 
   const removeBook = (e)=>{
@@ -111,35 +106,44 @@ const BooksQuery = (props)=> {
     addBook();
     getBooks();
   }
-  console.log(name, author, 'book')
     return (
         <div>
-        <Form >
-          <FormGroup>
-            <Label for="exampleEmail">Name</Label>
-            <Input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} id="bookName" placeholder="Name of book" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="examplePassword">Author</Label>
-            <Input type="text" value={author} name="authorId" onChange={(e) => setAuthor(e.target.value)} id="examplePassword" placeholder="Author" />
-          </FormGroup>
-          <Button color="success" onClick={addBooks}>Submit</Button>
-        </Form>
+        <div style={{display:'grid', placeItems:'center'}}>
+          <Form inline>
+            <h2>Add Books</h2>
+            <FormGroup>
+              <Label for="name" hidden>Name</Label>
+              <Input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} id="bookName" placeholder="Name of book" />
+            </FormGroup>
+            {' '}
+            <FormGroup>
+              <Label for="author" hidden>Author</Label>
+              <Input type="number" value={author} name="authorId" onChange={(e) => setAuthor(e.target.value)} id="examplePassword" placeholder="Author" />
+            </FormGroup>
+            {' '}
+            <p></p>
+            <Button color="success" onClick={addBooks}>Submit</Button>
+          </Form>
+        </div>
 
-        <Modal isOpen={modal} toggle={toggle} className={className} unmountOnClose={unmountOnClose}>
+        <Modal isOpen={modal} toggle={toggle} unmountOnClose={unmountOnClose}>
           <ModalHeader toggle={toggle}>Update</ModalHeader>
           <ModalBody>
-            <FormGroup>
-              <Label for="exampleEmail">ID</Label>
-              <Input type="text" name="name" value={updateId} onChange={(e) => setUpdateId(e.target.value)} id="bookName" placeholder="Name of book" />
+            <FormGroup disabled>
+              <Label for="id">ID</Label>
+              <Input type="text" name="name" value={updateId} onChange={(e) => setUpdateId(e.target.value)} id="bookId" placeholder="" disabled/>
             </FormGroup>
             <FormGroup>
-              <Label for="exampleEmail">Name</Label>
-              <Input type="text" name="name" value={updateName} onChange={(e) => setUpdateName(e.target.value)} id="bookName" placeholder="Name of book" />
+              <Label for="name">Name</Label>
+              <Input type="text" name="name" value={updateName} onChange={(e) => setUpdateName(e.target.value)} id="bookName" placeholder="" />
             </FormGroup>
             <FormGroup>
-              <Label for="examplePassword">Author</Label>
-              <Input type="text" value={updateAuthor} name="authorId" onChange={(e) => setUpdateAuthor(e.target.value)} id="examplePassword" placeholder="Author" />
+              <Label for="authorId">Author Id</Label>
+              <Input type="number" value={updateAuthorId} name="authorId" onChange={(e) => setUpdateAuthorId(e.target.value)} id="authorId" placeholder="" />
+            </FormGroup>
+            <FormGroup disabled>
+              <Label for="authorName">Author Name</Label>
+              <Input type="text" value={updateAuthor} name="authorName" onChange={(e) => setUpdateAuthor(e.target.value)} id="authorName" placeholder="" disabled/>
             </FormGroup>
           </ModalBody>
           <ModalFooter>
@@ -148,13 +152,13 @@ const BooksQuery = (props)=> {
           </ModalFooter>
         </Modal>
 
-        <Table>
+        <Table responsive>
           <thead>
             <tr>
-              <th>ID</th>
+              <th>#</th>
               <th>NAME</th>
+              <th>AUTHOR ID</th>
               <th>AUTHOR</th>
-              <th></th>
               <th></th>
             </tr>
           </thead>
@@ -163,9 +167,9 @@ const BooksQuery = (props)=> {
             return <tr key={book.id}>
                     <td>{book.id && book.id}</td>
                     <td>{book.name && book.name}</td>
+                    <td>{book.author && book.author.id}</td>
                     <td>{book.author && book.author.name}</td>
-                    <td><Button color="primary" onClick={toggle}>update</Button></td>
-                    <td><Button color="danger" id={book.id} onClick={(event) => removeBook(event)}>remove</Button></td>
+                  <td><Button color="primary" onClick={toggle}>update</Button> <Button color="danger" id={book.id} onClick={(event) => removeBook(event)}>remove</Button></td>
                   </tr>
             })
           }
